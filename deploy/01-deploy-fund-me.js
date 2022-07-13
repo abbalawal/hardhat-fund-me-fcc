75c8320434d4af2d3067e6077014ad5be50f82d2
Aminu Abba <aminuabba@pop-os.localdomain>
2022-07-13 12:06:56 +0100

    initial commit

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,7 @@
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    // when going for localhost or hardhat network we want to use a mock
+}
